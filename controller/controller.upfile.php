072a8a5772f852928a664b0a9f40791735af4fbf
Lane391 <robinwong51@qq.com>
2017-03-23 19:16:48 +0800

    modify for php7

--- controller/controller.upfile.php
+++ controller/controller.upfile.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by iResearch
  * User: JOSON
@@ -13,26 +14,29 @@ class UpfileController extends Controller
     private $model;
     const M = "Upfile";
 
-    public function __construct()
+    public function __construct($className)
     {
+        parent::__construct($className);
         $this->model = Model::instance(self::M);
     }
 
-    public function index(){
+    public function index()
+    {
         $ret_data = array(
-            'resTime' => time().'',
+            'resTime' => time() . '',
             'data' => '',
             'resCode' => '000002',
             'resMsg' => '请求地址错误!!!'
         );
-        echo json_encode($ret_data,true);
+        echo json_encode($ret_data, true);
     }
 
     //上传图片
-    public function imgs(){
+    public function imgs()
+    {
         //接收头像信息
         $where = json_decode(file_get_contents('php://input'), true);
-        $ret_data = array( 'resTime' => time().'', 'data' => '', 'resCode' => '', 'resMsg' => '' );//初始返回值
+        $ret_data = array('resTime' => time() . '', 'data' => '', 'resCode' => '', 'resMsg' => '');//初始返回值
 
         //图片二进制
         $filebase64 = $where['filebase64'];
@@ -40,26 +44,25 @@ class UpfileController extends Controller
         $filetype = $where['filetype'];
 
         //验证图片类型
-        if($filetype === 'png' or $filetype === 'jpg' or $filetype === 'jpeg') { } else {
+        if ($filetype !== 'png' or $filetype !== 'jpg' or $filetype !== 'jpeg') {
             $ret_data['resCode'] = '000002';
             $ret_data['resMsg'] = '只支持jpg、png格式';
-            echo json_encode($ret_data,JSON_UNESCAPED_UNICODE);
+            echo json_encode($ret_data, JSON_UNESCAPED_UNICODE);
         }
 
         //上传图片
-        $ret = upFile($filebase64,$filetype,"head/");
+        $ret = upFile($filebase64, $filetype, "head/");
 
         //验证是否上传成功
-        if($ret['retfile']=='')
-        {
+        if ($ret['retfile'] == '') {
             $ret_data['resCode'] = '000003';
             $ret_data['resMsg'] = '上传失败';
-            echo json_encode($ret_data,JSON_UNESCAPED_UNICODE);
+            echo json_encode($ret_data, JSON_UNESCAPED_UNICODE);
         } else {
             $ret_data['resCode'] = '000000';
             $ret_data['resMsg'] = '上传成功';
             $ret_data['data']['imageUrl'] = $ret['filepath'];
-            echo json_encode($ret_data,JSON_UNESCAPED_UNICODE);
+            echo json_encode($ret_data, JSON_UNESCAPED_UNICODE);
         }
     }
 
